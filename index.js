a1e039b0d82d98a27f1c2da57c58dcd2807b4000
Mayank-Singh-Bhati <121754832+Mayank-Singh-Bhati@users.noreply.github.com>
2023-09-28 13:23:12 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-// // Add this JavaScript to your existing JavaScript code
-const hamburgerMenu = document.getElementById('hamburgerMenu');
-const navLinks = document.getElementById('navLinks');
-
-hamburgerMenu.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-    hamburgerMenu.classList.toggle('active');
-});
